5d1954264dce3a91ad608b73b35d78598fa8717d
Paninee Chaiyaworn <56478923+MllAlli@users.noreply.github.com>
2020-12-29 17:03:05 +0700

    update calendar

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -19,18 +19,26 @@ export default function Calendar() {
     return value.isSame(day, "day")
   }
 
+  /*
   function beforeToday(day){
     return day.isBefore(new Date(), "day")
   }
+  */
 
   function isToday(day){
     return day.isSame(new Date(), "day")
   }
 
+  function isSameMonth(day){
+    return day.isSame(new Date(), "month")
+  }
+
   function dayStyles(day){
-    if (beforeToday(day)) return "before"
+    
     if (isSelected(day)) return "selected"
     if(isToday(day)) return "today"
+    if(isSameMonth(day) === false) return "notSameMonth"
+    if(isSameMonth(day)) return "sameMonth"
     return ""
   }
 
