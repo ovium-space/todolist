472b45df8c3232b319e4d7fb831973def36c5ef2
Paninee Chaiyaworn <56478923+MllAlli@users.noreply.github.com>
2020-12-29 15:17:39 +0700

    update file address

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calendar from ".";
+import Calendar from "./calendar/calendar.js";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
