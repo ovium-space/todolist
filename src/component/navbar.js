4ee8b2b01c770f750ac62966462022e120ad06b3
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2021-01-05 11:20:15 +0700

    update Navbar

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -4,8 +4,9 @@ import React from 'react';
 function Navbar() {
     return (
     <nav className="navbar">
-        <h1 className="title">User's To Do List </h1>
-        <div className="displayImg">OP</div>
+        <span className="navbar-brand h1">User's To Do List </span>
+        <img class="rounded-circle" width="50" alt="50x50" src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
+          data-holder-rendered="true"/>
     </nav>
     );
   }
