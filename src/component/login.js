2652e0d9dfdd953c3df32b044e502ef564aacc43
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2021-01-05 16:06:30 +0700

    Create login

---
+++ src/component/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../style/login.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function login() {
+
+  return (
+    <div className="box-login">
+      
+    </div>
+  );
+}
+
+export default login;
