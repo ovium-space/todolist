e3278bb196af80f5ac0accf0e36fd67ae33d8dee
kanjanaw <56288314+kanjanaw@users.noreply.github.com>
2021-01-06 14:59:17 +0700

    Update style

--- src/component/test.js
+++ src/component/test.js
@@ -8,15 +8,36 @@ function Test(props) {
     return (
       <div key={"checklist" + index}>
         <div class="form-check m-3">
-          <input id={checker.name} class="form-check-input" type="checkbox"/>
+          <div className="row">
+            <div className="col-5">
+              <input id={checker.name} class="form-check-input" type="checkbox"/>
+              <input
+                id={checker.name}
+                className="form-control form-control-sm ml-2"
+                type="text"
+                placeholder="Enter Checklist Name"
+                for={checker.name}
+                id="input-grey"
+              />
+            </div>
+            <div className="col-7">
+              <div className="row">
+                <label for="time-picker" className="col-3">
+                  Deadline : 
+                </label>
+              
+                  <input
+                    id="time-picker"
+                    className="form-control col-4 input-grey"
+                    type="datetime-local"
+                    id="input-grey"
+                  />
+              </div>
+            
+                
+            </div>
+          </div>
         </div>
-        <input
-          id={checker.name}
-          className="form-control form-control-sm ml-2"
-          type="text"
-          placeholder="Enter Checklist Name"
-          for={checker.name}
-        />
       </div>
     );
   });
@@ -40,34 +61,41 @@ function Test(props) {
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           <form>
-            <div className="form-group row m-1">
-              <div>
+            <div className="form-group row">
+              <div className="col-7">
                 <input
-                  className="form-control"
+                  className="form-control-lg"
                   type="text"
                   placeholder="Enter your Todo Title"
+                  id="input-grey"
                 />
               </div>
-              <div className="form-group row">
-                <label for="time-picker" className="col-sm-4 col-form-label">
+             
+              <div className="col-5">
+                <div className="row">
+                  <label for="time-picker" className="col-4">
                   Expired
                 </label>
-                <div className="col-sm-8">
+                <div className="col-8">
                   <input
                     id="time-picker"
                     class="form-control"
                     type="datetime-local"
+                    id="input-grey"
                   />
                 </div>
+                </div>
+                
               </div>
             </div>
-            <div className="form-group row m-1">
-              <div>
+            <div className="form-group row">
+              <div className="ml-3 col-8">
                 <input
                   id="description"
                   className="form-control form-control-sm"
                   type="text"
                   placeholder="Enter your Todo Description"
+                  id="input-grey"
                 />
               </div>
             </div>
@@ -79,10 +107,11 @@ function Test(props) {
           {Checklist}
           <Button
             onClick={() => AddChecklist()}
-            variant="outline-warning"
+            variant="secondary"
             size="sm"
+            className="btn-add-checklist ml-2 mt-0"
           >
-            Add checklist
+            Add Checklist
           </Button>
         </form>
       </Modal.Body>
